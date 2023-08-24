faaefa8471d837d94192591d5c425fc97a1ca3f0
Lucas Braga Santos <lucasbrasantos@gmail.com>
2023-08-24 11:18:13 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise'); 
 
-const bd_usuario = 'ewerton_node'; // usuário
-const bd_senha = '123456'; // senha
+const bd_usuario = 'us_etim_121_g2'; // usuário
+const bd_senha = 'ec0623'; // senha
 const bd_servidor = '10.67.22.216'; // servidor
 const bd_porta = '3306'; // porta
-const bd_banco = 'ewerton_node'; // nome do banco
+const bd_banco = 'bd_tcc_etim_121_g2'; // nome do banco
 let connection;
     
 const config = {
